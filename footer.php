c0ffd9e0ab92eff91e9ce1f3eb855c8aefa6bba0
trumpl <37820513+trumpl@users.noreply.github.com>
2018-04-02 14:34:21 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer class="footer-wrap" id="footer">
+    <div class="footer-box clearfix">
+        <div class="ft-left fl tl">
+            <ul class="link-list">
+                <li class="classify">友情链接</li>
+                <li><a href="http://www.thinkwithu.com/" target="_blank">申友网</a></li>
+                <li><a href="http://www.toeflonline.cn/" target="_blank">雷哥网托福</a></li>
+                <li><a href="http://www.gmatonline.cn/" target="_blank">雷哥网GMAT在线</a></li>
+                <li><a href="http://www.smartapply.cn/" target="_blank">留学商城</a></li>
+            </ul>
+            <ul class="link-list">
+                <li class="classify">雅思产品</li>
+                <li><a href="/cn/course.html#md-1">雅思基础课程</a></li>
+                <li><a href="/cn/course.html#md-2">雅思强化课程</a></li>
+                <li><a href="/cn/course.html#md-3">雅思冲刺课程</a></li>
+                <li><a href="/cn/course.html#md-4">雅思一对一课程</a></li>
+            </ul>
+        </div>
+        <div class="ft-ewm">
+            <ul class="ewm-list clearfix">
+                <li>
+                    <div class="ermImg">
+                        <img src="/cn/images/index-code.jpg" alt="">
+                    </div>
+                    <p class="ermName">雷哥网雅思小助手</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+  <p>本网站所提供的知识库内容，部分来源于雷哥IELTS整理发布，版权归雷哥网所有，仅供学习者交流免费使用；部分来源于互联网，版权归原作者所有，仅供学习者交流免费使用。</p>
+  <p>本网站保留在法律允许的范围内对本声明的解释权，并有权随时修改本声明，请注意随时关注本声明的内容。</p>
+</footer>
